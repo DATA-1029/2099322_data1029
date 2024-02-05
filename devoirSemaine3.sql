b6d0711572301ead02bbdc9e98485bc59e1c2448
danpoi01 <poirier.danick@yahoo.ca>
2024-02-05 18:53:53 -0400

    Question5

--- devoirSemaine3.sql
+++ devoirSemaine3.sql
@@ -11,3 +11,6 @@ select au_fname, au_lname, pub_name from library.authors as la right join librar
 
 -- 4. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant les auteurs et les éditeurs qui ne répondent pas à ce critère.
 select au_fname, au_lname, pub_name from library.authors as la left join library.publishers as lp on lp.city = la.city union select au_fname, au_lname, pub_name from library.authors as la right join library.publishers as lp on lp.city = la.city;
+
+-- 5. Effectif(nombre) d'employes par niveau d'experience.
+select count(*), job_lvl from employees group by job_lvl;
