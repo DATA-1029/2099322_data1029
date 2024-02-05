bbd213e5c9cc24fc816cbe3139693d7262504417
danpoi01 <poirier.danick@yahoo.ca>
2024-02-05 18:48:55 -0400

    Question2

--- devoirSemaine3.sql
+++ devoirSemaine3.sql
@@ -2,3 +2,6 @@ use library;
 
 -- 1. La liste des paires (auteur, éditeur) demeurant dans la même ville.
 select au_fname, au_lname, pub_name from library.authors as la join library.publishers as lp where lp.city = la.city;
+
+-- 2. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant aussi les auteurs qui ne répondent pas à ce critère.
+select au_fname, au_lname, pub_name from library.authors as la left join library.publishers as lp on lp.city = la.city;
