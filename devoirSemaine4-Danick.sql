c6f900467fcbc2e0d0dffde2161e1597b1fb7014
danpoi01 <poirier.danick@yahoo.ca>
2024-02-14 20:46:52 -0400

    Question3

--- devoirSemaine4-Danick.sql
+++ devoirSemaine4-Danick.sql
@@ -21,6 +21,21 @@ WHERE a.au_id IN(
 	WHERE pub_id IN(
 		SELECT pub_id
         FROM publishers
-        WHERE pub_name = "Eyerolles"
+        WHERE pub_name = "Eyrolles"
+    )
+);
+
+-- 3. Obtenir la liste des noms d’auteurs ayant touché une avance supérieure à toutes les avances versées par l'éditeur "Harmattan".
+SELECT au_fname, au_lname
+FROM authors
+WHERE au_id IN(
+    SELECT au_id
+    FROM titleauthor AS ta
+    JOIN titles as t ON ta.title_id = t.title_id
+    WHERE advance > ALL(
+        SELECT advance
+        FROM titles AS t
+        JOIN publishers AS p ON t.pub_id = p.pub_id
+        WHERE pub_name = 'Harmattan'
     )
 );
