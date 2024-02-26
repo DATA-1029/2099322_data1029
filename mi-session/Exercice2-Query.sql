49d29b64db5067c2acaf40206255c053e4309d5f
danpoi01 <poirier.danick@yahoo.ca>
2024-02-26 14:43:06 -0400

    queries

---
+++ mi-session/Exercice2-Query.sql
@@ -0,0 +1,13 @@
+use epharmacy;
+-- Exercice 2
+-- 1. La liste des utilisateurs de l’application selon leur rôle.
+
+
+
+-- 2. Noms et quantités des produits achetés par Oumar Moussa.
+
+
+
+
+
+-- 3. Quel sont les noms de produits dont le fournisseur est basé à Moncton ?
