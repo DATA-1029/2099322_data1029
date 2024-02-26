527d9b60ae3001bd0d1754c511854ad455984f87
danpoi01 <poirier.danick@yahoo.ca>
2024-02-26 14:53:20 -0400

    question1

--- mi-session/Exercice2-Query.sql
+++ mi-session/Exercice2-Query.sql
@@ -1,7 +1,9 @@
 use epharmacy;
 -- Exercice 2
 -- 1. La liste des utilisateurs de l’application selon leur rôle.
-
+SELECT u.full_name AS 'Nom complet', r.name AS 'Rôle'
+FROM `user` u
+JOIN `role` r ON u.role_id = r.id;
 
 
 -- 2. Noms et quantités des produits achetés par Oumar Moussa.
