cc69114acdf31a1d29463f42d0ae20f07a39f367
danpoi01 <poirier.danick@yahoo.ca>
2024-03-22 23:15:37 -0300

    Question2

--- devoirSemaine6.sql
+++ devoirSemaine6.sql
@@ -4,3 +4,13 @@ SELECT e.fname, e.lname, p.pub_name FROM employees AS e
 JOIN
 publishers AS p ON p.pub_id = e.pub_id
 WHERE e.job_lvl = 'SEINIOR' ORDER BY p.pub_id DESC;
+
+
+-- 2. Noms complets des employés ayant un salaire supérieur à celui de Norbert Zongo.
+
+SELECT fname, lname FROM employees AS e 
+WHERE salary>(
+	SELECT salary FROM employees 
+	WHERE fname = 'Norbert' 
+	AND lname = 'Zongo'
+);
